1be0383b5bc463d1086f700e274d14f743d25aae
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-02-09 22:29:09 -0500

    Bump version: 0.1.1 → 0.2.0

--- version.go
+++ version.go
@@ -24,7 +24,7 @@ package main
 const Name string = "sss"
 
 // Version defines current version number.
-const Version string = "0.1.1"
+const Version string = "0.2.0"
 
 // Author defines the author of this program.
 const Author string = "Junpei Kawamoto"
